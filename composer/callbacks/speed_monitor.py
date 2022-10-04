bff35c385b769fc73dce931912e0ee4a312f08fd
Brandon Cui <bcui@Brandons-MacBook-Pro.local>
2022-10-04 16:35:51 -0700

    Adjusting speedmonitor to only utilize batch_end as the endpoint, should match HF

--- composer/callbacks/speed_monitor.py
+++ composer/callbacks/speed_monitor.py
@@ -96,31 +96,41 @@ class SpeedMonitor(Callback):
         )
         self.total_eval_wct = state['total_eval_wct']
 
-    def batch_start(self, state: State, logger: Logger) -> None:
-        del logger  # unused
-        self.batch_start_wct = state.timestamp.total_wct.total_seconds()
-        self.batch_start_num_samples = int(state.timestamp.sample)
+    # def batch_start(self, state: State, logger: Logger) -> None:
+    # del logger  # unused
+    # self.batch_start_wct = state.timestamp.total_wct.total_seconds()
+    # self.batch_start_num_samples = int(state.timestamp.sample)
 
     def batch_end(self, state: State, logger: Logger):
-        batch_num_samples = int(state.timestamp.sample) - self.batch_start_num_samples
-        batch_wct = state.timestamp.total_wct.total_seconds() - self.batch_start_wct
+        if self.batch_start_num_samples == 0:
+            self.set_batch_starting_stats()
+        else:
+            batch_num_samples = int(state.timestamp.sample) - self.batch_start_num_samples
+            batch_wct = state.timestamp.total_wct.total_seconds() - self.batch_start_wct
 
-        # Add the new element
-        self.batch_wct_buffer.append(batch_wct)
-        self.batch_num_samples_buffer.append(batch_num_samples)
+            # Resetting it for the next batch as done in HF
+            self.set_batch_starting_stats()
 
-        # Log the throughput
-        if len(self.batch_num_samples_buffer) == self.window_size:
-            throughput = sum(self.batch_num_samples_buffer) / sum(self.batch_wct_buffer)
-            logger.log_metrics({'throughput/samples_per_sec': throughput})
+            # Add the new element
+            self.batch_wct_buffer.append(batch_wct)
+            self.batch_num_samples_buffer.append(batch_num_samples)
 
-        # Log the time
-        # `state.timestamp` excludes any time spent in evaluation
-        logger.log_metrics({
-            'wall_clock/train': state.timestamp.total_wct.total_seconds(),
-            'wall_clock/val': self.total_eval_wct,
-            'wall_clock/total': (state.timestamp.total_wct.total_seconds() + self.total_eval_wct),
-        })
+            # Log the throughput
+            if len(self.batch_num_samples_buffer) == self.window_size:
+                throughput = sum(self.batch_num_samples_buffer) / sum(self.batch_wct_buffer)
+                logger.log_metrics({'throughput/samples_per_sec': throughput})
+
+            # Log the time
+            # `state.timestamp` excludes any time spent in evaluation
+            logger.log_metrics({
+                'wall_clock/train': state.timestamp.total_wct.total_seconds(),
+                'wall_clock/val': self.total_eval_wct,
+                'wall_clock/total': (state.timestamp.total_wct.total_seconds() + self.total_eval_wct),
+            })
+
+    def set_batch_starting_stats(self, state: State) -> None:
+        self.batch_start_wct = state.timestamp.total_wct.total_seconds()
+        self.batch_start_num_samples = int(state.timestamp.sample)
 
     def eval_end(self, state: State, logger: Logger):
         del logger  # unused
